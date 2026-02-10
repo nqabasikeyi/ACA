3d2915bd693c0aa53c70d6a44b7dba988bef6a91
Nqaba Sikeyi <100336686+nqabasikeyi@users.noreply.github.com>
2026-02-10 07:15:21 +0200

    Refactor Footer component and update social icons

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,25 +1,22 @@
-import React from 'react';
-import InstagramIcon from '@mui/icons-material/Instagram';
-import TwitterIcon from '@mui/icons-material/Twitter';
-import FacebookIcon from '@mui/icons-material/Facebook';
-
+import React from "react";
+import { FaTiktok } from "react-icons/fa";
+import TwitterIcon from "@mui/icons-material/Twitter";
+import FacebookIcon from "@mui/icons-material/Facebook";
 
 function Footer() {
   return (
     <footer className="text-center text-lg-start text-muted">
       <section className="d-flex justify-content-center justify-content-lg-between p-4 border-bottom">
-        <div className="me-5 d-none d-lg-block">
-          
-        </div>
-        <div className=''>
-          
-        </div>
+        <div className="me-5 d-none d-lg-block"></div>
+        <div className=""></div>
       </section>
       <section className="">
         <div className="container text-center text-md-start mt-5">
           <div className="row mt-3">
             <div className="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4">
-              <h6 className="text-uppercase fw-bold mb-4">Amazon Christian Academy</h6>
+              <h6 className="text-uppercase fw-bold mb-4">
+                Amazon Christian Academy
+              </h6>
               <p>
                 Address: <br />
                 31, Filabusi <br />
@@ -29,35 +26,63 @@ function Footer() {
             </div>
             <div className="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
               <h6 className="text-uppercase fw-bold mb-4">Contact Us</h6>
-              <p>
-                Email: amazonchristianacademy@gmail.com
-              </p>
+              <p>Email: amazonchristianacademy@gmail.com</p>
+
               <h6 className="text-uppercase fw-bold mb-3 mt-3">Follow Us</h6>
-              <a href="" target='_blank' className='text-3xl text-pink-700'>
-                <InstagramIcon />
-              </a>
-              <a href="" target='_blank' className='text-3xl text-blue-500 ml-3'>
-                <TwitterIcon />
-              </a>
-              <a href="https://www.facebook.com/profile.php?id=61566837965521" target='_blank' className='text-3xl text-blue-700 ml-3'>
-                <FacebookIcon />
-              </a>
+
+              <div className="d-flex align-items-center gap-3">
+                <a
+                  href="https://www.tiktok.com/@amazonchristianacademy"
+                  target="_blank"
+                  rel="noreferrer"
+                  className="text-pink-700"
+                >
+                  <FaTiktok size={20} />
+                </a>
+
+                <a
+                  href=""
+                  target="_blank"
+                  rel="noreferrer"
+                  className="text-blue-500"
+                >
+                  <TwitterIcon fontSize="small" />
+                </a>
+
+                <a
+                  href="https://www.facebook.com/profile.php?id=61566837965521"
+                  target="_blank"
+                  rel="noreferrer"
+                  className="text-blue-700"
+                >
+                  <FacebookIcon fontSize="small" />
+                </a>
+              </div>
             </div>
+
             <div className="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
               <h6 className="text-uppercase fw-bold mb-4">Support</h6>
-              <div className='flex flex-col gap-3'>
+              <div className="flex flex-col gap-3">
                 <p>
                   Name: Mr Nathan Kuchena <br />
                   Phone: +263717788812
                 </p>
-                
+                <p>
+                  Name: Mrs N Mlangeni
+                  <br />
+                  Phone: +263772703804
+                </p>
               </div>
             </div>
           </div>
         </div>
       </section>
-      <div className="text-center p-4" style={{ backgroundColor: "rgba(0, 0, 0, 0.05)" }}>
-        © {new Date().getFullYear()}  Amazon Christian Academy. All rights reserved.
+      <div
+        className="text-center p-4"
+        style={{ backgroundColor: "rgba(0, 0, 0, 0.05)" }}
+      >
+        © {new Date().getFullYear()} Amazon Christian Academy. All rights
+        reserved.
       </div>
     </footer>
   );
